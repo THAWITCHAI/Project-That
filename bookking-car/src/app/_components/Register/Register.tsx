3e30d7778377b6a9f16e48faaa41f40233b3d056
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-08-13 01:56:32 +0700

    Register

--- bookking-car/src/app/_components/Register/Register.tsx
+++ bookking-car/src/app/_components/Register/Register.tsx
@@ -9,110 +9,110 @@ export default function Register({}: Props) {
   return (
     <div className="register flex justify-center items-center">
       <div className="w-1/2 h-full p-10 regis-form">
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center Image-box">
           <Image className="Image" src={"/add-user.png"} width={128} height={128} alt="" />
         </div>
-        <form className="from" action="" method="post">
-          <div className="input-register ">
-            <label className="text-white text-xl" htmlFor="">
+        <form className="from border" action="" method="post">
+          <div className="input-register border">
+            <label className="text-white text-xl border" htmlFor="">
               ชื่อ
             </label>
             <input
               required
-              className="-none focus:outline-none text-white"
+              className="-none focus:outline-none text-white border"
               type="text"
               placeholder="Name"
             />
           </div>
-          <div className="input-register ">
-            <label className="text-white text-xl" htmlFor="">
+          <div className="input-register border">
+            <label className="text-white text-xl border" htmlFor="">
               นามสกุล
             </label>
             <input
               required
-              className="-none focus:outline-none text-white"
+              className="-none focus:outline-none text-white border"
               type="text"
               placeholder="Last Name"
             />
           </div>
-          <div className="input-register ">
-            <label className="text-white text-xl" htmlFor="">
+          <div className="input-register border">
+            <label className="text-white text-xl border" htmlFor="">
               ชื่อเล่น
             </label>
             <input
               required
-              className="-none focus:outline-none text-white"
+              className="-none focus:outline-none text-white border"
               type="text"
               placeholder="Nick Name"
             />
           </div>
-          <div className="input-register ">
-            <label className="text-white text-xl" htmlFor="">
+          <div className="input-register border">
+            <label className="text-white text-xl border" htmlFor="">
               อีเมลล์
             </label>
             <input
               required
-              className="-none focus:outline-none text-white"
+              className="-none focus:outline-none text-white border"
               type="email"
               placeholder="Your Email"
             />
           </div>
-          <div className="input-register ">
-            <label className="text-white text-xl" htmlFor="">
+          <div className="input-register border">
+            <label className="text-white text-xl border" htmlFor="">
               รหัสผ่าน
             </label>
             <input
               required
-              className="-none focus:outline-none text-white"
+              className="-none focus:outline-none text-white border"
               type="password"
               placeholder="Password"
             />
           </div>
           <div className="input-register ">
-            <label className="text-white text-xl" htmlFor="">
+            <label className="text-white text-xl border" htmlFor="">
               ยืนยันรหัสผ่าน
             </label>
             <input
               required
-              className="-none focus:outline-none text-white"
+              className="-none focus:outline-none text-white border"
               type="password"
               placeholder="Confirm Password"
             />
           </div>
-          <div className="input-register ">
-            <label className="text-white text-xl" htmlFor="">
+          <div className="input-register border">
+            <label className="text-white text-xl border" htmlFor="">
               เบอร์โทร
             </label>
             <input
               required
-              className="-none focus:outline-none text-white"
+              className="-none focus:outline-none text-white border"
               type="text"
               placeholder="Phone Number"
             />
           </div>
-          <div className="input-register file ">
-            <label className="text-white text-xl" htmlFor="">
+          <div className="input-register file border">
+            <label className="text-white text-xl border" htmlFor="">
               ใบขับขี่
             </label>
             <input
               required
-              className="input-1 -none focus:outline-none text-white"
+              className="input-1 -none focus:outline-none text-white border"
               type="file"
               placeholder="Name"
             />
           </div>
           <div className="input-register file">
-            <label className="text-white text-xl" htmlFor="">
+            <label className="text-white text-xl border" htmlFor="">
               ภาพโปรไฟล์
             </label>
             <input
               required
-              className="input-1 -none  focus:outline-none text-white "
+              className="input-1 -none  focus:outline-none text-white border"
               type="file"
               placeholder="Name"
             />
           </div>
-          <div className=" button flex justify-center ">
+          <div className=" button flex justify-center border">
             <div className="w-1/2 ">
               <span
               >
